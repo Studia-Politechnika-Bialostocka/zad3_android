fae452aac57d16d9ab13d81c780f7651aac00fa9
MateuszBialowas <mateusz.bialowas2@gmail.com>
2021-11-09 17:12:13 +0100

    done 14

---
+++ app/src/main/java/com/mateusz/zad3_android/SingleFragmentActivity.java
@@ -0,0 +1,7 @@
+package com.mateusz.zad3_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SingleFragmentActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/mateusz/zad3_android/TaskListActivity.java
@@ -0,0 +1,13 @@
+package com.mateusz.zad3_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
