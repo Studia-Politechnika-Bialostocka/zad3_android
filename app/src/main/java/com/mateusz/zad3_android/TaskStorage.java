050349e38d7e3c3119673b73525a1c6894bddd39
MateuszBialowas <mateusz.bialowas2@gmail.com>
2021-11-09 17:04:15 +0100

    done 11

---
+++ app/src/main/java/com/mateusz/zad3_android/TaskStorage.java
@@ -0,0 +1,22 @@
+package com.mateusz.zad3_android;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskStorage {
+    private static final TaskStorage taskStorage = new TaskStorage();
+
+    private final List<Task> tasks;
+
+    public static TaskStorage getInstance() { return taskStorage; }
+
+    private TaskStorage() {
+        tasks = new ArrayList<>();
+        for (int i = 1; i <= 100; i++){
+            Task task = new Task();
+            task.setName("Zadanie #" + i);
+            task.setDone(i % 3 == 0);
+            tasks.add(task);
+        }
+    }
+}
